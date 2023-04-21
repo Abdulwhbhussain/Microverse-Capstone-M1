0b0087615c59a289462a2af4093953d80e4aa811
WHB901 <WHB901@gmail.com>
2023-04-21 09:50:26 +0500

    Removed Linter Errors

--- indexAbout.js
+++ indexAbout.js
@@ -1,13 +1,13 @@
-const aboutpage = document.querySelector('#aboutpage');
+// const aboutpage = document.querySelector('#aboutpage');
 
 const hamburger = document.querySelector('#hamburger-menu');
 
 const crossInMenu = document.querySelector('#close-menu');
 
-hamburger.addEventListener('click', (e) => {
-    document.querySelector('#menu').style.display = 'block';
+hamburger.addEventListener('click', () => {
+  document.querySelector('#menu').style.display = 'block';
 });
 
-crossInMenu.addEventListener('click', (e) => {
-    document.querySelector('#menu').style.display = 'none';
+crossInMenu.addEventListener('click', () => {
+  document.querySelector('#menu').style.display = 'none';
 });
